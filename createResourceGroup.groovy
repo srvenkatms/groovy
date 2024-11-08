60c0f1442cd198d73648270568b2f8c60dcbdef6
srvenkatms <66653353+srvenkatms@users.noreply.github.com>
2024-11-08 07:38:50 -0800

    Update createResourceGroup.groovy

--- createResourceGroup.groovy
+++ createResourceGroup.groovy
@@ -1,25 +1,10 @@
-node {
-    def resourceGroupName = "myResourceGroup"
-    def location = "EastUS" // Update the region as per your requirement
-
-    stage('Login to Azure') {
-        // Login to Azure using a service principal
-        sh '''
-        az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
-        '''
-    }
-
-    stage('Create Resource Group') {
-        // Create the resource group using Azure CLI
-        sh """
-        az group create --name $resourceGroupName --location $location
-        """
-    }
-
-    stage('Verify Resource Group Creation') {
-        // Verify if the resource group was created successfully
-        sh """
-        az group show --name $resourceGroupName
-        """
-    }
+// createResourceGroup.groovy
+def createAzureResourceGroup(resourceGroupName, location) {
+    sh """
+    az login --service-principal -u "$AZURE_CLIENT_ID" -p "$AZURE_CLIENT_SECRET" --tenant "$AZURE_TENANT_ID"
+    az group create --name ${resourceGroupName} --location ${location}
+    az group show --name ${resourceGroupName}
+    """
 }
+
+return this
